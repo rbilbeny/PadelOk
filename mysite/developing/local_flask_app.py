05082bd17276d8e3e6cfbcdd14c1927441e5cefd
rbilbeny <105988537+rbilbeny@users.noreply.github.com>
2023-01-23 18:09:46 -0300

    Changed Search object to address only single_searches
    It also gave the responsability of building the club to the main app

--- mysite/developing/local_flask_app.py
+++ mysite/developing/local_flask_app.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent.parent))
 from search import ClubSearch
+from club import Club
 
 
 def handle_request_post_clubs(input_text):
@@ -21,13 +22,14 @@ def handle_request_post_clubs(input_text):
 
 
 
-def handle_request_get_single_scraper(club_id, initial_date, final_date, inital_time, final_time): 
+def handle_request_get_single_scraper(club_id, date, inital_time, final_time): 
     with open("/Users/rodrigobilbeny/Documents/GitHub/PadelOk/mysite/clubs.json", 'r') as clubs:
         lines = clubs.readlines()
         line = lines[0]
     
     #LOCAL EQUALS WEB
-    single_club_search = ClubSearch(line, club_id, initial_date, final_date, inital_time, final_time)
+    club = Club(line, club_id)
+    single_club_search = ClubSearch(club, date, inital_time, final_time)
     single_club_search.scrape()
     search_result_list = []
     for court in single_club_search.result:
@@ -35,3 +37,22 @@ def handle_request_get_single_scraper(club_id, initial_date, final_date, inital_
 
     json_courts = json.dumps(search_result_list, indent=4)
     return json_courts
+
+'''
+def handle_request_get_multi_scraper(clubs_ids, initial_date, final_date, inital_time, final_time): 
+    with open("/Users/rodrigobilbeny/Documents/GitHub/PadelOk/mysite/clubs.json", 'r') as clubs:
+        lines = clubs.readlines()
+        line = lines[0]
+    
+    #LOCAL EQUALS WEB
+            start_date = datetime.strptime(self.initial_date, "%d/%m/%Y")
+            end_date = datetime.strptime(self.final_date, "%d/%m/%Y")
+            delta = timedelta(days=1)
+            current_date = start_date
+            while current_date <= end_date:
+                self.result.extend(tcp_scraper(self.club, current_date.strftime("%d/%m/%Y"), self.initial_time, self.final_time))
+                current_date += delta   
+
+    json_courts = json.dumps(multisearch_result_list, indent=4)
+    return json_courts
+'''    
--- mysite/search.py
+++ mysite/search.py
@@ -1,62 +1,24 @@
-from datetime import datetime, timedelta 
-
-from club import Club
 from tcpmatchpoint_scraper import scraper as tcp_scraper
-
-
-class LocationSearch:
-    """
-    Creates a Search that defines the scope of the court web scrapping based on a broad location, and performs it
-    """
-
-    def __init__(self, location_list, location_type, initial_date, final_date, inital_time, final_time):
-        
-        if location_type == "zones":
-            self.location_attr = "zone_name_text"
-        elif location_type == "districts":
-            self.location_attr = "district_text"
-
-        self.locations = location_list
-        self.initial_date = initial_date
-        self.final_date = final_date
-        self.initial_time = inital_time
-        self.final_time = final_time
-        self.result = list()
-
 
 class ClubSearch:
     """
     Creates a Search that defines the scope of the court web scrapping for 1 club, and performs it
     """
 
-    def __init__(self, clubs_text, club_id, initial_date, final_date, inital_time, final_time):
+    def __init__(self, club, date, inital_time, final_time):
         
-        self.club = Club(clubs_text, club_id)
-        self.initial_date = initial_date
-        self.final_date = final_date
+        self.club = club
+        self.date = date
         self.initial_time = inital_time
         self.final_time = final_time
         self.result = list()
 
     def scrape(self):
         if self.club.web_scraper == "tcpmatchpoint":
-            start_date = datetime.strptime(self.initial_date, "%d/%m/%Y")
-            end_date = datetime.strptime(self.final_date, "%d/%m/%Y")
-            delta = timedelta(days=1)
-            current_date = start_date
-            while current_date <= end_date:
-                self.result.extend(tcp_scraper(self.club, current_date.strftime("%d/%m/%Y"), self.initial_time, self.final_time))
-                current_date += delta
+            self.result = tcp_scraper(self.club, self.date, self.initial_time, self.final_time)
 
         else:
             return    
-
-
-
-
-        
-         
-
 
 
         
