6531e9a0cdda4d46884c57f6d82ae354c17c84a3
rbilbeny <105988537+rbilbeny@users.noreply.github.com>
2023-01-24 20:02:52 -0300

    fix request size for posting clubs

---
+++ mysite/developing/tcpmatchpoint_id_finder.py
@@ -0,0 +1,160 @@
+import requests
+import json
+from datetime import datetime, timedelta
+
+
+
+PROXY_ACTIVE = False
+API_KEY = ""
+
+def get_session(calendar_url):
+
+	headers = {
+		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+		'Accept-Language': 'en-US,en;q=0.9,ar-TN;q=0.8,ar;q=0.7',
+		'Cache-Control': 'no-cache',
+		'Connection': 'keep-alive',
+		'Pragma': 'no-cache',
+		'Upgrade-Insecure-Requests': '1',
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+	}
+
+	while True:
+		try:
+			if PROXY_ACTIVE:
+				response = requests.get("https://api.scrapestack.com/scrape?access_key="+API_KEY+"&render_js=0&url="+calendar_url, headers=headers, verify=False)
+			else:
+				response = requests.get(calendar_url, headers=headers, verify=False)
+			if response.status_code==200:
+				break
+		except:
+			pass
+
+	html=response.text
+
+	cheat_code=html.split("heatCode='")[1].split("';")[0]
+
+	session_id=response.cookies.get_dict()["ASP.NET_SessionId"]
+
+	return session_id,cheat_code
+
+
+
+def get_id(id_url,session_id,cheat_code):
+
+	cookies = {
+		'ASP.NET_SessionId': session_id,
+		'i18next': 'es-CL',
+		'MPOpcionCookie': 'necesarios',
+	}
+
+	headers = {
+		'Accept': 'application/json, text/javascript, */*; q=0.01',
+		'Accept-Language': 'en-US,en;q=0.9,ar-TN;q=0.8,ar;q=0.7',
+		'Cache-Control': 'no-cache',
+		'Connection': 'keep-alive',
+		'Content-Type': 'application/json; charset=UTF-8',
+		'Pragma': 'no-cache',
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+		'X-Requested-With': 'XMLHttpRequest',
+	}
+
+	json_data = {
+		'p': cheat_code,
+	}
+
+	while True:
+		try:
+			if PROXY_ACTIVE:
+				response = requests.post("https://api.scrapestack.com/scrape?access_key="+API_KEY+"&render_js=0&url="+id_url,cookies=cookies,headers=headers,json=json_data,verify=False)
+			else:
+				response = requests.post(id_url,cookies=cookies,headers=headers,json=json_data,verify=False)
+			if response.status_code==200:
+				break
+
+		except:
+			pass
+
+	id=response.json()["d"][0]["Id"]
+
+	return id
+
+
+
+def get_calendar(API_url,calendar_url,session_id,cheat_code,id,date):
+
+	cookies = {
+	'ASP.NET_SessionId': session_id,
+	'i18next': 'es-CL',
+	'MPOpcionCookie': 'necesarios',
+	}
+
+	headers = {
+		'Accept': 'application/json, text/javascript, */*; q=0.01',
+		'Accept-Language': 'en-US,en;q=0.9,ar-TN;q=0.8,ar;q=0.7',
+		'Cache-Control': 'no-cache',
+		'Connection': 'keep-alive',
+		'Content-Type': 'application/json; charset=UTF-8',
+		'Pragma': 'no-cache',
+		'Referer': calendar_url,
+		'Sec-Fetch-Dest': 'empty',
+		'Sec-Fetch-Mode': 'cors',
+		'Sec-Fetch-Site': 'same-origin',
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+		'X-Requested-With': 'XMLHttpRequest',
+		'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+		'sec-ch-ua-mobile': '?0',
+		'sec-ch-ua-platform': '"Windows"',
+	}
+
+	json_data = {
+		'idCuadro': id,
+		'fecha': date,
+		'p': cheat_code,
+	}
+
+	if PROXY_ACTIVE:
+		response = requests.post("https://api.scrapestack.com/scrape?access_key="+API_KEY+"&render_js=0&url="+API_url,cookies=cookies,headers=headers,json=json_data,verify=False)
+	else:
+		response = requests.post(API_url,cookies=cookies,headers=headers,json=json_data,verify=False)
+
+	calendar=response.json()
+
+	return calendar
+
+
+
+def price_to_int(price):
+
+	if price is None or price == "":
+		return 0
+	price = price.replace("$","")
+	price = price.replace(".","")
+	price = price.replace(",",".")
+
+	return int(price)
+
+
+
+url_base = "https://www.clubconecta.cl"
+url_path_scraper = "/Pages/15-CLUB_CONECTA_LA_DEHESA_PADEL"
+calendar_url = url_base + url_path_scraper
+id_url = url_base + "/booking/srvc.aspx/ObtenerCuadros"
+API_url = url_base + "/booking/srvc.aspx/ObtenerCuadro"
+
+nmax = 50
+n=0
+while n < nmax:	
+
+	print("URL:",calendar_url)
+	
+	session_id,cheat_code = get_session(calendar_url)
+
+
+	id = n
+	print("id forzado:",str(id))
+	calendar = get_calendar(API_url, calendar_url, session_id, cheat_code, id,"25/1/2023")
+	club_ = calendar["d"]["Nombre"]
+	print("nombre del club:",club_)
+	n = n+1
+	
--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route('/post_clubs', methods=['POST'])
 def handle_request_post_clubs():
-    input_text = str(request.args.get('clubs'))
+    input_text = str(request.get_data())
     input_text = input_text.replace('\\', "")
     with open("/home/rodrigobilbeny/mysite/clubs.json", 'w') as clubs:
         clubs.write(input_text)
@@ -45,7 +45,7 @@ def handle_request_get_single_scraper():
     search_result_list = []
     for court in single_club_search.result:
         search_result_list.append(court.__dict__)
-    
+
     json_courts = json.dumps(search_result_list, indent=4)
     return json_courts
 
